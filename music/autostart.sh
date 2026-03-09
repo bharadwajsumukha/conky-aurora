d03daf83dd990e5ccdb1463b2ab48e902b0553bb
rew62 <rew222@gmail.com>
2026-03-09 17:01:47 -0400

    inital commit music conky/lua script

--- music/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Change directory to the script's location
-cd "$(dirname "$0")" || exit
-
-#sleep 2  # only if launching at login
-
-( set -x; setsid bash nowplaying/start.sh & )
-#( set -x; setsid bash eq/start.sh & )
-#( set -x; setsid bash lyrics/start.sh & )
